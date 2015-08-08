92ccc863320b309465dccc51d48f5b1a4de36425
zafio <j@zafio.com>
2015-08-08 02:01:45 +0200

    Simplified code and added comments

--- Frame_Hotkeys_v0_2.py
+++ Frame_Hotkeys_v0_2.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'Frame hotkeys',
-    'author': 'Julio Iglesias',
-    'version': (0, 2),
+    'author': 'Julio Iglesias, Adrian Novoa',
+    'version': (0, 21),
     'blender': (2, 75, 2),
     'location': '1/2 (framerate), W/E (previous/next frame), Q (toggle preview range) at Timeline, Graph & Dopesheet editors',
     'warning': '',
@@ -12,6 +12,7 @@ bl_info = {
 
 import bpy
 
+# Increases framerate by 5 or the closest value to make it multiple of 5
 class RENDER_OT_set_fps_increase(bpy.types.Operator):
     '''FPS Increase'''
     bl_idname = "render.set_fps_increase"
@@ -21,23 +22,19 @@ class RENDER_OT_set_fps_increase(bpy.types.Operator):
 
         scene = bpy.context.scene
 
-        if context.area.type != 'VIEW_3D' and scene.render.fps == 24:
-            scene.render.fps += 1
+        fps = int(scene.render.fps)
 
-        elif context.area.type != 'VIEW_3D' and scene.render.fps == 20:
+        if context.area.type != 'VIEW_3D' and fps % 5 == 0:
             scene.render.fps += 5
 
-        elif context.area.type != 'VIEW_3D' and scene.render.fps >= 25:
-            scene.render.fps += 5
-
-        elif context.area.type != 'VIEW_3D' and scene.render.fps <= 1:
-            scene.render.fps += 4
-
-        elif context.area.type != 'VIEW_3D' and scene.render.fps <= 20:
-            scene.render.fps += 5
+        elif context.area.type != 'VIEW_3D' and fps % 5 != 0:
+            mod = fps % 5
+            toAdd = 5 - mod
+            scene.render.fps += toAdd
 
         return {'FINISHED'}
 
+# Decreases framerate by 5 or the closest value to make it multiple of 5
 class RENDER_OT_set_fps_decrease(bpy.types.Operator):
     '''FPS decrease'''
     bl_idname = "render.set_fps_decrease"
@@ -47,20 +44,18 @@ class RENDER_OT_set_fps_decrease(bpy.types.Operator):
 
         scene = bpy.context.scene
 
-        if context.area.type != 'VIEW_3D' and scene.render.fps == 24:
-            scene.render.fps -= 4
+        fps = int(scene.render.fps)
 
-        elif context.area.type != 'VIEW_3D' and scene.render.fps == 25:
+        if context.area.type != 'VIEW_3D' and fps % 5 == 0:
             scene.render.fps -= 5
 
-        elif context.area.type != 'VIEW_3D' and scene.render.fps <= 20:
-            scene.render.fps -= 5
-
-        elif context.area.type != 'VIEW_3D' and scene.render.fps >= 30:
-            scene.render.fps -= 5
+        elif context.area.type != 'VIEW_3D' and fps % 5 != 0:
+            mod = fps % 5
+            scene.render.fps -= mod
 
         return {'FINISHED'}
 
+# Toggles preview range at timeline ON or OFF
 class TIMELINE_OT_toggle_preview_range(bpy.types.Operator):
     '''Toggle Preview Range'''
     bl_idname = "timeline.toggle_preview_range"
@@ -78,6 +73,7 @@ class TIMELINE_OT_toggle_preview_range(bpy.types.Operator):
 
         return {'FINISHED'}
 
+#Jumps to next frame, or first frame when current frame equals to end frame
 class TIMELINE_OT_next_frame(bpy.types.Operator):
     '''Next Frame'''
     bl_idname = "timeline.next_frame"
@@ -101,6 +97,7 @@ class TIMELINE_OT_next_frame(bpy.types.Operator):
 
         return {'FINISHED'}
 
+#Jumps to previous frame
 class TIMELINE_OT_previous_frame(bpy.types.Operator):
     '''Next Frame'''
     bl_idname = "timeline.previous_frame"
