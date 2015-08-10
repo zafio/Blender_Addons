f6ac2973bff9e267000faa9c6fd957f6cefbbb9a
zafio <j@zafio.com>
2015-08-10 20:03:51 +0200

    added hotkeys to cycle through action list

--- Frame_Hotkeys_v0_3.py
+++ Frame_Hotkeys_v0_3.py
@@ -1,16 +1,92 @@
 bl_info = {
     'name': 'Frame hotkeys',
     'author': 'Julio Iglesias, Adrian Novoa',
-    'version': (0, 21),
-    'blender': (2, 75, 2),
-    'location': '1/2 (framerate), W/E (previous/next frame), Q (toggle preview range) at Timeline, Graph & Dopesheet editors',
+    'version': (0, 3),
+    'blender': (2, 75, 4),
+    'location':  'Timeline, Graph, Dopesheet: W/E (prev/next frame), 1/2 (framerate), D/F (prev/next action), Q (toggle preview range)',
     'warning': '',
-    'description': 'Adds hotkeys to Increase/Decrease Framerate, Previous/Next Frame and toggle Preview Range',
+    'description': 'Adds hotkeys to Increase/Decrease Framerate, Previous/Next Frame, Action and toggle Preview Range',
     'wiki_url': '',
     'tracker_url': '',
     'category': 'Render'}
 
 import bpy
+
+# Jumps to next action in list, sets preview range based on its first and last keyframes (so it works with no bones selected)
+class DOPESHEET_OT_next_action(bpy.types.Operator):
+    '''Dopesheet Next Action'''
+    bl_idname = "dopesheet.next_action"
+    bl_label = "Dopesheet Next Action"
+
+    def execute(self, context):
+
+        ob = bpy.context.object
+        act = bpy.data.actions # list of actions
+        actlen = len(act) # length of action list
+        scene = bpy.context.scene
+
+        if ob.animation_data.action == None and actlen >= 1:
+            ob.animation_data.action = act[0]
+            acran = ob.animation_data.action.frame_range
+            scene.frame_preview_start = acran[0]
+            scene.frame_preview_end = acran[1]
+
+        elif ob.animation_data.action == act[actlen-1]:
+            ob.animation_data.action = act[0]
+            acran = ob.animation_data.action.frame_range
+            scene.frame_preview_start = acran[0]
+            scene.frame_preview_end = acran[1]
+
+        elif ob.animation_data.action != None:
+            for i in range(0,actlen-1):
+                if ob.animation_data.action == act[i]:
+                    ob.animation_data.action = act[i+1]
+                    acran = ob.animation_data.action.frame_range
+                    scene.frame_preview_start = acran[0]
+                    scene.frame_preview_end = acran[1]
+                    break
+
+        return {'FINISHED'}
+
+# Jumps to previous action in list, sets preview range based on its first and last keyframes (so it works with no bones selected)
+class DOPESHEET_OT_previous_action(bpy.types.Operator):
+    '''Dopesheet Previous Action'''
+    bl_idname = "dopesheet.previous_action"
+    bl_label = "Dopesheet previous Action"
+
+    def execute(self, context):
+
+        ob = bpy.context.object
+        act = bpy.data.actions # list of actions
+        actlen = len(act) # length of action list
+        scene = bpy.context.scene
+        option = bpy.ops.action
+
+        if ob.animation_data.action == None and actlen >= 1:
+            ob.animation_data.action = act[0]
+            acran = ob.animation_data.action.frame_range
+            scene.frame_preview_start = acran[0]
+            scene.frame_preview_end = acran[1]
+
+
+        elif ob.animation_data.action == act[0]:
+            ob.animation_data.action = act[actlen-1]
+            acran = ob.animation_data.action.frame_range
+            scene.frame_preview_start = acran[0]
+            scene.frame_preview_end = acran[1]
+
+
+        elif ob.animation_data.action != None:
+            for i in range(actlen-1,0,-1):
+                if ob.animation_data.action == act[i]:
+                    ob.animation_data.action = act[i-1]
+                    acran = ob.animation_data.action.frame_range
+                    scene.frame_preview_start = acran[0]
+                    scene.frame_preview_end = acran[1]
+
+                    break
+
+        return {'FINISHED'}
 
 # Increases framerate by 5 or the closest value to make it multiple of 5
 class RENDER_OT_set_fps_increase(bpy.types.Operator):
@@ -21,7 +97,6 @@ class RENDER_OT_set_fps_increase(bpy.types.Operator):
     def execute(self, context):
 
         scene = bpy.context.scene
-
         fps = int(scene.render.fps)
 
         if context.area.type != 'VIEW_3D' and fps % 5 == 0:
@@ -123,6 +198,40 @@ class TIMELINE_OT_previous_frame(bpy.types.Operator):
 
 def register():
 
+    bpy.utils.register_class(DOPESHEET_OT_next_action)
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Timeline", space_type="TIMELINE")
+        kmi = km.keymap_items.new('dopesheet.next_action', 'F', 'PRESS')
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Graph Editor", space_type="GRAPH_EDITOR")
+        kmi = km.keymap_items.new('dopesheet.next_action', 'F', 'PRESS')
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Dopesheet", space_type="DOPESHEET_EDITOR")
+        kmi = km.keymap_items.new('dopesheet.next_action', 'F', 'PRESS')
+
+    bpy.utils.register_class(DOPESHEET_OT_previous_action)
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Timeline", space_type="TIMELINE")
+        kmi = km.keymap_items.new('dopesheet.previous_action', 'D', 'PRESS')
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Graph Editor", space_type="GRAPH_EDITOR")
+        kmi = km.keymap_items.new('dopesheet.previous_action', 'D', 'PRESS')
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Dopesheet", space_type="DOPESHEET_EDITOR")
+        kmi = km.keymap_items.new('dopesheet.previous_action', 'D', 'PRESS')
+
     bpy.utils.register_class(RENDER_OT_set_fps_increase)
 
     kc = bpy.context.window_manager.keyconfigs.addon
@@ -209,6 +318,57 @@ def register():
         kmi = km.keymap_items.new('timeline.previous_frame', 'W', 'PRESS')
 
 def unregister():
+
+    bpy.utils.unregister_class(DOPESHEET_OT_next_action)
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Timeline"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.next_action':
+                km.keymap_items.remove(kmi)
+                break
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Graph Editor"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.next_action':
+                km.keymap_items.remove(kmi)
+                break
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Dopesheet"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.next_action':
+                km.keymap_items.remove(kmi)
+                break
+
+    bpy.utils.unregister_class(DOPESHEET_OT_previous_action)
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Timeline"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.previous_action':
+                km.keymap_items.remove(kmi)
+                break
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Graph Editor"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.previous_action':
+                km.keymap_items.remove(kmi)
+                break
+
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps["Dopesheet"]
+        for kmi in km.keymap_items:
+            if kmi.idname == 'dopesheet.previous_action':
+                km.keymap_items.remove(kmi)
+                break
+
     bpy.utils.unregister_class(RENDER_OT_set_fps_increase)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
@@ -218,7 +378,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(RENDER_OT_set_fps_increase)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Graph Editor"]
@@ -227,7 +386,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(RENDER_OT_set_fps_increase)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Dopesheet"]
@@ -245,7 +403,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(RENDER_OT_set_fps_decrease)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Graph Editor"]
@@ -254,7 +411,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(RENDER_OT_set_fps_decrease)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Dopesheet"]
@@ -272,7 +428,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_toggle_preview_range)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Graph Editor"]
@@ -281,7 +436,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_toggle_preview_range)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Dopesheet"]
@@ -299,7 +453,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_next_frame)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Graph Editor"]
@@ -308,7 +461,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_next_frame)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Dopesheet"]
@@ -326,7 +478,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_previous_frame)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Graph Editor"]
@@ -335,7 +486,6 @@ def unregister():
                 km.keymap_items.remove(kmi)
                 break
 
-    bpy.utils.unregister_class(TIMELINE_OT_previous_frame)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["Dopesheet"]
